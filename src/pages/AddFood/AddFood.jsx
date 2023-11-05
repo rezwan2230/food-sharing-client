4fc3176c8e4a41738dc8970e287ed67f31aa7a18
rezwan2230 <rezwan2230@gmail.com>
2023-11-06 02:20:12 +0600

    added add food page

--- src/pages/AddFood/AddFood.jsx
+++ src/pages/AddFood/AddFood.jsx
@@ -1,61 +1,80 @@
+import { useContext } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
 
 const AddFood = () => {
+
+    const {user} = useContext(AuthContext)
+    const authorName = user?.displayName
+    const authorPhotoUrl = user?.photoURL
+
+
+    const handleAddFood =(e)=>{
+        e.preventDefault()
+        const form = e.target
+        const foodName = form.foodName.value
+        const foodImg = form.foodImage.value
+        const quantity = form.quantity.value
+        const pickupLocation = form.location.value
+        const price = form.price.value
+        const expiredate = form.date.value
+        const additionalNotes = form.notes.value
+        console.log(authorName, authorPhotoUrl, foodName, foodImg, quantity, pickupLocation, price, expiredate, additionalNotes);
+
+        
+
+        
+
+
+    }
     return (
-        <div className="w-full  font-sans bg-cover" style={{backgroundImage : 'url(https://i.ibb.co/1Myc997/pexels-kamshotthat-7465827.jpg)', backgroundSize:'fit', backgroundPosition: 'center', objectFit:"top left" }}>
-            
-        <section className="container p-6 mx-auto glass rounded-md shadow-md   mt-10" >
-            <h2 className="text-4xl font-semibold  capitalize text-white text-center mb-10">Add Product</h2>
-            
-            <form >
-                <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2 px-10 ">
-                    <div>
-                        <label className="text-white">Name</label>
-                        <input type="text" name="name" placeholder="product name" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
-                    </div>
+        <div className="w-full  font-sans bg-cover" style={{ backgroundImage: 'url(https://i.ibb.co/1Myc997/pexels-kamshotthat-7465827.jpg)', backgroundSize: 'fit', backgroundPosition: 'center', objectFit: "top left" }}>
 
-                    <div>
-                        <label className="text-white" >Type</label>
-                        <input id="password" type="text" name="type" placeholder="product type" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
-                    </div>
+            <section className="container p-6 mx-auto glass rounded-md shadow-md   mt-10" >
+                <h2 className="text-4xl font-semibold  capitalize text-white text-center mb-10">Add Product</h2>
 
-                    <div>
-                        <label className="text-white">Price</label>
-                        <input id="passwordConfirmation" type="number" name="price" placeholder="product price" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
-                    </div>
+                <form onSubmit={handleAddFood}>
+                    <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2 px-10 ">
+                        <div>
+                            <label className="text-white">Food Name</label>
+                            <input type="text" name="foodName" placeholder="Food name" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
+                        </div>
 
-                    <div>
-                        <label className="text-white">Rating</label>
-                        <input id="passwordConfirmation" type="number" name="rating" placeholder="product rating" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
-                    </div>
+                        <div>
+                            <label className="text-white">Food Image</label>
+                            <input id="password" type="text" name="foodImage" placeholder="Food imgage" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
+                        </div>
 
-                    <div>
-                        <label className="text-white">Photo URL</label>
-                        <input id="passwordConfirmation" type="text" name="photo" placeholder="product img url" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
-                    </div>
+                        <div>
+                            <label className="text-white">Quantity</label>
+                            <input id="passwordConfirmation" type="number" name="quantity" placeholder="quantity" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
+                        </div>
 
-                    <div>
-                        <label className="text-white" >Brand Name</label>
-                        <br></br>
-                        <select name="brandname" id="" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required>
-                            <option value="Lamborghini">Lamborghini</option>
-                            <option value="BMD">BMD</option>
-                            <option value="Toyota">Toyota</option>
-                            <option value="AUDI">AUDI</option>
-                            <option value="Mercedes-Benz">Mercedes-Benz</option>
-                            <option value="Nissan">Nissan</option>
-                        </select>
+                        <div>
+                            <label className="text-white">Pickup Location</label>
+                            <input id="passwordConfirmation" type="text" name="location" placeholder="location" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
+                        </div>
+
+                        <div>
+                            <label className="text-white">Price</label>
+                            <input id="passwordConfirmation" type="text" name="price" placeholder="Price" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
+                        </div>
+
+                        <div>
+                            <label className="text-white">Expire Date</label>
+                            <input id="passwordConfirmation" type="date" name="date" placeholder="Expire date" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" required></input>
+                        </div>
+
                     </div>
-                </div>
-                <div className="relative mb-4 mt-5 px-10 ">
-                    <label className="leading-7 text-white">Short Description</label>
-                    <textarea id="message" name="message" className="mt-2 w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required></textarea>
-                </div>
-                <div className="flex justify-end mt-8 mb-10 mr-9">
-                    <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Add Product</button>
-                </div>
-            </form>
-        </section>
-    </div>
+                    <div className="relative mb-4 mt-5 px-10 ">
+                        <label className="leading-7 text-white">Additional Notes</label>
+                        <textarea id="message" name="notes" className="mt-2 w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required></textarea>
+                    </div>
+                    <div className="flex justify-end mt-8 mb-10 mr-9">
+                        <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Add Food</button>
+                    </div>
+                </form>
+            </section>
+        </div>
     );
 };
 
--- src/pages/Home/FeaturedSection/FeaturedSection.jsx
+++ src/pages/Home/FeaturedSection/FeaturedSection.jsx
@@ -1,63 +1,63 @@
 import '../FeaturedSection/FeaturedSection.css'
+import badge from '../../../assets/medal.png'
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../../provider/AuthProvider';
 
 const FeaturedSection = () => {
+    const {user} = useContext(AuthContext)
     return (
-        // <div>
-        //     <div className="card">
-        //         <div className="card-details">
-        //             <p className="text-title">Card Title</p>
-        //             <p className="text-body">Card Details</p>
-        //         </div>
-        //         <a className="card-button" href="#link">More info</a>
-        //     </div>
-
-
-
-        <div className="w-[450px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-
-            <img className="rounded-t-lg w-full h-80" src="https://i.ibb.co/1Myc997/pexels-kamshotthat-7465827.jpg" alt="" />
+        <div className="relative w-[440px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <img className="rounded-t-lg w-full h-72 z-1" src="https://i.ibb.co/64rztwh/pizza-pizza-filled-with-tomatoes-salami-olives.jpg" alt="" />
+            <div>
+                <img className='h-12 w-12 absolute top-5 right-2 z-8' src={badge} alt="" />
+                <p className='absolute text-black  rounded-xl top-6 right-[15px] z-10  font-bold text-center text-sm'><span className='text-lg text-red-700'>15</span>%<br /></p>
+            </div>
 
             <div className="p-5 pt-2">
-                {/* <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Pizza</h5> */}
-                <p className="-mt-16 absolute max-w-fit px-3 py-1 text-lg font-semibold tracki bg-white  bg-opacity-75 rounded ">Pizza</p>
-
-                <div className='flex items-center justify-between divide-x-2 divide-neutral-950'>
-                    <div className="flex items-center mt-6 flex-1">
+                <p className="-mt-16 absolute max-w-fit px-3 py-1 text-lg font-semibold tracki bg-white bg-opacity-75 rounded ">Pizza</p>
+                <div className='flex items-center justify-between '>
+                    <div className="flex items-center mt-2">
                         <img className="object-cover object-center w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="" />
-                        <div className="mx-4">
-                            <h1 className="text-sm text-gray-700 dark:text-gray-200">Amelia. Anderson</h1>
-                            <p className="text-sm text-gray-500 dark:text-gray-400">Lead Developer</p>
+                        <div className="mx-4 font-semibold">
+                            <h1 className="text-lg text-gray-700 dark:text-gray-200">Amelia. Anderson</h1>
+                            <p className="text-sm text-gray-500 dark:text-gray-400">Owner of FoodPanda</p>
                         </div>
                     </div>
 
-                    
-
-                    <div className='mt-7 mr-10 pl-16'>
+                    <div className='mt-2 text-lg font-semibold'>
                         <p>Quantity : 5</p>
                     </div>
                 </div>
 
+                <p className="mt-3 font-normal text-gray-700 dark:text-gray-400">Be sure to warm it up in the oven before eating. Delivery is given within 30 min.</p>
 
-
-
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-
-                <div>
-                    <div>
+                <div className='flex justify-between my-2 text-lg font-semibold'>
+                    <div >
                         <p>Location : Gaibandha</p>
                     </div>
                     <div>
                         <p>Expire Date : 25-5-1900</p>
                     </div>
                 </div>
+                <div className='flex justify-center gap-5 py-2'>
+                    <div className='flex'>
+                        <Link className="inline-flex items-center px-6 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Request
+                        </Link>
+                    </div>
+                    <div className='flex'>
+                        <Link href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </Link>
+                    </div>
+
+                </div>
 
 
-                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                </a>
             </div>
         </div>
 
